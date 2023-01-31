e2d5c47b8bd7515af59fc745f45473b012135bc0
Joseph Steffin <jsteffin@presidio.com>
2023-01-31 19:36:14 +0530

    initial-commmit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: ``,
-})
-export class App {
-  name = 'Angular';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
 }
 
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
